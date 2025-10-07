3944ed0bfb37f9c7d87e7a30ab0edf646a7e0bdc
studywithankit <studywithankitalok@gmail.com>
2025-10-07 02:52:55 +0000

    date review

---
+++ basics/date.js
@@ -0,0 +1,14 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23, 5, 5)
+console.log(myCreatedDate.toLocaleDateString());
+
+
