92e572b44db8eced7d72217e0c3c19418661d9d2
studywithankit <studywithankitalok@gmail.com>
2025-10-07 15:42:33 +0000

    this is the first array file

--- basics/date.js
+++ basics/date.js
@@ -8,9 +8,32 @@
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
-let myCreatedDate = new Date(2023, 0, 23, 5, 5)
-console.log(myCreatedDate.toLocaleDateString());
-console.log(myCreatedDate.toLocaleDateString());
+// let myCreatedDate = new Date(2023, 0, 23, 5, 5)
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate = new Date("01-14-2023")
+
+// let myTimeStamp = Date.now()
+
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getMonth() + 1);
+
+
+// newDate.toLocaleString('default', {
+
+//     weekday : "long",
+//     timeZone: 
+// })
+
+// console.log(newDate);
+
 
 
 
