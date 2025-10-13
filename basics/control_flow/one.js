3dbaac798d32a43b37eb7ca2b2216f05f9be9132
studywithankit <studywithankitalok@gmail.com>
2025-10-13 21:57:33 +0530

    if changes

--- basics/control_flow/one.js
+++ basics/control_flow/one.js
@@ -1,1 +1,9 @@
-console.log("Ankit Kumar");
+// if
+
+const isUserloggedIn = true;
+
+if (true) {
+} // (= => assignment operator), (== => cheack equal), !(= => not equal), (=== => cheack equal and also type of the data)
+
+if (true) {
+}
