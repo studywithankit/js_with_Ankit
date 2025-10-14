27fd4a348f855013d609ef34e91d37b1102c506d
studywithankit <studywithankitalok@gmail.com>
2025-10-14 21:57:35 +0530

    switch

---
+++ basics/control_flow/switch.js
@@ -0,0 +1,54 @@
+// Basic Syntax for switch
+
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("fabuary");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("july");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    break;
+}
