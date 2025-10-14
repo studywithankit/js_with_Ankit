47637d0d1936524b6a9e408a8ebfa2283e0c25fb
studywithankit <studywithankitalok@gmail.com>
2025-10-14 20:19:12 +0530

    else if and nested if-else

--- basics/control_flow/one.js
+++ basics/control_flow/one.js
@@ -2,8 +2,43 @@
 
 const isUserloggedIn = true; // (= => assignment operator)
 
-if (true) {
-} // (= => assignment operator), (== => cheack equal), !(= => not equal), (=== => cheack equal and also type of the data)
+const temperature = 41;
 
-if (true) {
+if (temperature < 50) {
+  // console.log("less than 50");
+} else {
+  //console.log("temperature is greater than 50");
+}
+
+// if (true) {
+// } // (= => assignment operator), (== => cheack equal), !(= => not equal), (=== => cheack equal and also type of the data), (!== => Strict Inequality)
+
+// if (true) {
+// }
+
+// if-else general format
+
+const score = 50;
+if (score > 100) {
+  let power = "Ankit";
+  //console.log(`user power: ${power}`);
+} else {
+  let power = "Alok";
+  //console.log(`user power: ${power}`);
+}
+
+//if-else shorthand notation
+
+const balance = 1000;
+//if (balance > 500) console.log("test"),console.log("test2"); //never type this type of code it is good practice
+
+//nested if-else if
+if (balance < 1500) {
+  // console.log("less than 1500");
+} else if (balance < 500) {
+  // console.log("less than 600");
+} else if (balance < 500) {
+  // console.log("less than 400");
+} else {
+  // console.log("less than 500");
 }
